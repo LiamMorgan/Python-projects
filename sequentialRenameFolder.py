d39508d35c580f8e11e90d63c3143a77c1eef50e
Liam Morgan <liammorgan128@gmail.com>
2018-03-22 02:21:58 +0000

    Add files via upload

--- sequentialRenameFolder.py
+++ sequentialRenameFolder.py
@@ -1,4 +1,4 @@
-#Renames all files in a folder to 0-x ordered by their creation date
+#Renames all files in a folder to 1-x ordered by their creation date
 import os
 
 def sorted_dir(folder):
@@ -12,7 +12,7 @@ i = 0
 for file_name in sorted_dir(path):
     _, ext = os.path.splitext(file_name)
     print (file_name + " - " + str(i)+ext)    
-    os.rename(os.path.join(path,file_name), os.path.join(path, str(i) + ".jpg"))
+    os.rename(os.path.join(path,file_name), os.path.join(path, str(i) + ext)
     i += 1
     
 print(str(i-1) + " files.")
